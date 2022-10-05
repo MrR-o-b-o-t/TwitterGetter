66226f32f921779acb23f1ae7d7ddfce013c18f7
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2022-10-04 22:08:26 -0500

    Update App.css, ad main content to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
-import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
+import { MDBContainer, MDBRow, MDBCol, MDBIcon } from "mdb-react-ui-kit";
 import React from "react";
 
 export default function Home() {
   return (
-    <MDBContainer fluid className="g-0">
+    <MDBContainer fluid className="g-0 mb-5">
       <div
         className="p-5 text-center bg-image"
         style={{
@@ -21,16 +21,49 @@ export default function Home() {
         </div>
       </div>
 
-      <MDBRow className="mt-2">
-        <MDBCol md="6">
-          <h3>Welcome to Twitter Getter</h3>
-          <p>This application was built with HTML, CSS, Javascript, NodeJS, React, and ExpressJs.</p>
-          <p>This is a simple application that consumes JSON data from the Twitter API based on user provided search parameters. After a user provides search parameters and the data has been returned it will 
-            be displayed in tweet format. The 'Search' page provides the 10 most recent Tweets while the 'Random' page will return 10 random Tweets from the past 2 years.
-          </p>
-        </MDBCol>
-        <MDBCol md="6"></MDBCol>
-      </MDBRow>
+      <MDBContainer fluid>
+        <MDBRow className="mt-5">
+          <MDBCol md="12" className="text-center">
+            <h2>Welcome to Twitter Getter</h2>
+            <h5 className="mt-3">
+              This application was built with HTML, CSS, Javascript, NodeJS,
+              React, and ExpressJs.
+            </h5>
+            <MDBRow className="mt-5 mb-4 text-center">
+              <MDBCol md="2" sm="6" className="mt-3">
+                <MDBIcon id="react-icon" fab icon="react" size="4x" />
+              </MDBCol>
+              <MDBCol md="2" sm="6" className="mt-3">
+                <MDBIcon id="html-icon" fab icon="html5" size="4x" />
+              </MDBCol>
+              <MDBCol md="2" sm="6" className="mt-3">
+                <MDBIcon id="css-icon" fab icon="css3" size="4x" />
+              </MDBCol>
+              <MDBCol md="2" sm="6" className="mt-3">
+                <MDBIcon id="js-icon" fab icon="js-square" size="4x" />
+              </MDBCol>
+              <MDBCol md="2" sm="6" className="mt-3">
+                <MDBIcon id="node-icon" fab icon="node-js" size="4x" />
+              </MDBCol>
+              <MDBCol md="2" sm="6" className="mt-3">
+                <MDBIcon id="mdb-icon" fab icon="mdb" size="4x" />
+              </MDBCol>
+            </MDBRow>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow className="mt-4 d-flex justify-content-center text-center">
+          <MDBCol lg="6">
+            <h5 className="">
+              This is a simple application that consumes JSON data from the
+              Twitter API based on user provided search parameters. After a user
+              provides search parameters and the data has been returned it will
+              be displayed in tweet format. The 'Search' page provides the 10
+              most recent Tweets while the 'Random' page will return 10 random
+              Tweets from the past 2 years.
+            </h5>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </MDBContainer>
   );
 }
--- src/pages/Random.js
+++ src/pages/Random.js
@@ -1,9 +1,36 @@
 import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+} from "mdb-react-ui-kit";
 
 export default function Random() {
   return (
-    <div>
-      <h1>Random page</h1>
-    </div>
+    <MDBContainer fluid className="pt-5">
+      <MDBRow className="d-flex justify-content-center">
+        <MDBCol sm="6">
+          <MDBCard>
+            <MDBCardImage
+              src="https://mdbootstrap.com/img/new/standard/nature/184.webp"
+              position="top"
+              alt="..."
+            />
+            <MDBCardBody>
+              <MDBCardTitle>User ID returned from Twitter API</MDBCardTitle>
+              <MDBCardText>
+                Some quick example text to build out the text retrieved from the
+                Twitter API.
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
